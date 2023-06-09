e9f55ba70fa218825347b22dcd9f9159dff1e171
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-06-09 11:10:36 +0000

    WIP

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -26,13 +26,8 @@ const getData = async (
     // gat all commits from repo
     const response = await octokit.rest.repos.listCommits({ owner, repo });
 
-    // filtered today changes file from all commits
-    const todayChangedFiles = getChangeFiles(response.data, today);
-    // filtered yesterday changes file from all commits
-    const yesterdayChangedFiles = getChangeFiles(response.data, yesterday);
-
     let commitData = [] as RecentData[];
-    for (const commit of response.data.slice(0,2)) {
+    for (const commit of response.data.slice(0,5)) {
       const latestCommitSHA = commit.sha;
       const commitResponse = await octokit.rest.repos.getCommit({
         owner: owner,
@@ -41,40 +36,15 @@ const getData = async (
       });
       commitData = commitData.concat(getLastChangeFile(commitResponse.data, paramsFilter));
     }
-    console.log(commitData);
 
-    // get today commit data
-    if (todayChangedFiles.length > 0) {
-      const latestCommitSHA = todayChangedFiles[0].sha;
-      const commitResponse = await octokit.rest.repos.getCommit({
-        owner: owner,
-        repo: repo,
-        ref: latestCommitSHA,
-      });
-      // get today necessary data for show list
-      todayData = getLastChangeFile(commitResponse.data, paramsFilter);
-    }
-
-    // get yesterday commit data
-    if (yesterdayChangedFiles.length > 0) {
-      const latestCommitSHA = yesterdayChangedFiles[0].sha;
-      const commitResponse: any = await octokit.rest.repos.getCommit({
-        owner: owner,
-        repo: repo,
-        ref: latestCommitSHA,
-      });
-
-      // get yesterday necessary data for show list
-      yesterdayData = getLastChangeFile(commitResponse.data, paramsFilter);
-    }
-    return { todayData, yesterdayData };
+    return { todayData: commitData };
   } catch (error: any) {
     throw new Error(error.message);
   }
 };
 
 const Home = async ({ searchParams }: { searchParams: { filter: string } }) => {
-  const { todayData, yesterdayData }: any = await getData(searchParams.filter);
+  const { todayData }: any = await getData(searchParams.filter);
   return (
     <section className="container max-w-5xl mx-auto ">
       <div className="my-10 ">
@@ -82,8 +52,6 @@ const Home = async ({ searchParams }: { searchParams: { filter: string } }) => {
         <Tab filtered={searchParams.filter} />
         {/* showing today changes  */}
         <ListContainer title="Today" data={todayData} />
-        {/* showing yesterday changes  */}
-        <ListContainer title="Yesterday" data={yesterdayData} />
       </div>
     </section>
   );
--- www/components/ListContainer.tsx
+++ www/components/ListContainer.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import { IoIosArrowForward } from "react-icons/io";
 import { FiFile } from "react-icons/fi";
+import { RecentData } from "@/types/RecentData";
+
 
 interface Props {
   data: any;
@@ -9,14 +11,23 @@ interface Props {
 }
 
 const ListContainer: React.FC<Props> = ({ data, title }) => {
+  let test : Object = {}
+  // Group by https://stackoverflow.com/a/40774906/9902555
+  test = data.reduce(function (r, a: RecentData) {
+        r[a.timestamp] = r[a.timestamp] || [];
+        r[a.timestamp].push(a);
+        return r;
+    }, Object.create(null));
+    console.log(Object.entries(test));
   return (
     <div className="mt-4">
+      {/* Iterate with Object.entries() */}
       <h2 className="text-green-700 font-bold  text-sm md:text-base  ">
         {title}
       </h2>
       <ul className="my-3 divide-y-2 text-sm md:text-base">
         {data.length > 0 ? (
-          data.map((item: any, i: number) => (
+          test.map((item: any, i: number) => (
             <li className="flex  py-2 px-4 bg-white" key={i}>
               <Link
                 href={item.filePath}
