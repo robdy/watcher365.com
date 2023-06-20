14e8b0087b865f6bc3b7015b1addbe03f83e41e5
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-06-20 17:27:14 +0200

    Uses default env variable for token (#26)

--- www/config/octokit.ts
+++ www/config/octokit.ts
@@ -2,5 +2,5 @@ import { Octokit } from "octokit";
 export const owner = 'robdy';
 export const repo = 'm365roadmap-history';
 export const octokit = new Octokit({
-  auth: process.env.GH_TOKEN,
+  auth: process.env.GITHUB_TOKEN,
 });
