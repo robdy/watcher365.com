7d955769aa4ff00359ba0ac8a3d361d57e2ac2c9
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-07-08 13:58:19 +0000

    Adds history script

---
+++ Get-EntriesHistory.ps1
@@ -0,0 +1,25 @@
+#region Settings and variables
+$dataFolder = 'data'
+$versionFolder = Join-Path $dataFolder 'versions'
+$timestamp = Get-Date -Format 'o'
+#endregion Settings and variables
+
+#region Processing
+$allFiles = Get-ChildItem $dataFolder -File
+foreach ($file in $allFiles) {
+	<#
+	$file = $allFiles[0]
+	#>
+	$gitLog = git log "data/$($file.Name)"
+	$commitsList = $gitLog | Select-String -Pattern 'commit \w{40}'
+
+	for ($i = $commitsList.count - 1; $i -gt 0; $i--) {
+		$beforeCommit = $commitsList[$i].Line.replace('commit ','')
+		$afterCommit  = $commitsList[$i-1].Line.replace('commit ','')
+		# Compare content from two commits
+		$beforeContent = git show "$($beforeCommit):data/$($file.Name)"
+		$afterContent  = git show "$($afterCommit):data/$($file.Name)"
+	}
+
+}
+#endregion Processing
