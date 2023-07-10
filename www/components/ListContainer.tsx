709da03ddd23f7502ee987124d6a279912993d39
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-07-10 06:35:25 +0000

    Configures Eslint

--- www/components/ListContainer.tsx
+++ www/components/ListContainer.tsx
@@ -10,24 +10,24 @@ const ListContainer: React.FC<Props> = ({ data }) => {
 
   // Group by https://stackoverflow.com/a/40774906/9902555
   const groupedData = data.reduce(function (r: any, a: RecentData) {
-    const shortTimestamp : string = a.timestamp.substring(0,10)
+    const shortTimestamp: string = a.timestamp.substring(0, 10)
     r[shortTimestamp] = r[shortTimestamp] || [];
     r[shortTimestamp].push(a);
     return r;
-    }, Object.create(null));
+  }, Object.create(null));
 
   return (
     <div className="mt-4">
       {Object.keys(groupedData).length > 0 ? (
         Object.keys(groupedData).map((item: any, i: number) => (
-          <React.Fragment>
-            <h2 className="text-green-700 font-bold text-sm md:text-base">
+          <React.Fragment key={`fragment-${item}`}>
+            <h2 className="text-green-700 font-bold text-sm md:text-base" key={`header-${item}`}>
               {item}
             </h2>
-            <ul className="my-3 divide-y-2 text-sm md:text-base">
+            <ul className="my-3 divide-y-2 text-sm md:text-base" key={`list-${item}`}>
               {groupedData[item].map((entry: any, j: number) => {
                 return entry ? (
-                  <EntryTile entryID={entry.filePath} commitData={entry} date={item}/>
+                  <EntryTile entryID={entry.filePath} commitData={entry} date={item} />
                 ) : (
                   <p>No local file found</p>
                 );
