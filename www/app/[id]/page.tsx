03d51acf2f3a197b09c81830d33a88e57b4da568
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-09-07 07:57:21 +0000

    Fixes typo in path

--- www/app/[id]/page.tsx
+++ www/app/[id]/page.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { repo, owner, octokit } from "@/config/octokit";
 import EntryTile from "@/components/EntryTile";
 const util = require('node:util');
 const exec = util.promisify(require('node:child_process').exec);
 
-async function lsExample() {
-  const { stdout, stderr } = await exec('git show 76522f74563225fbc8df53ec137d349ffdc55457:data/131169.json');
-  console.log('stdout:', stdout);
-  console.error('stderr:', stderr);
-}
-
 export async function generateStaticParams() {
   const { stdout, stderr } = await exec('ls ../data')
+  console.error('stderr:', stderr)
   const filesArr: string[] = stdout.split('\n')
 
   return filesArr.map((file) => ({
@@ -21,17 +15,14 @@ export async function generateStaticParams() {
 
 const EntryPage = async ({ params }: {params: {id: string}}) => {
   const { id } = params
-  const commitList = await octokit.rest.repos.listCommits({
-    owner,
-    repo,
-    path: `data/${id}.json`,
-  });
+  const { stdout } = await exec(`git log --format=format:%H ../data/${id}.json`)
+  const commitList: string[] = stdout.split('\n');
 
   return (
     <section className="container max-w-5xl mx-auto">
-      {commitList.data.map((item: any) => {
+      {commitList.map((item: any) => {
         return (
-        <EntryTile entryID={id} commitSha={item.sha} key={item.sha} />
+        <EntryTile entryID={id} commitSha={item} key={item} />
         )
       }
       )}
