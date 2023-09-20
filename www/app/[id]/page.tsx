76e0003c3e8e763d2e2298fcdc5f9866923abff4
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-09-20 16:52:00 +0000

    Adds dates to entry history pages

--- www/app/[id]/page.tsx
+++ www/app/[id]/page.tsx
@@ -15,14 +15,20 @@ export async function generateStaticParams() {
 
 const EntryPage = async ({ params }: {params: {id: string}}) => {
   const { id } = params
-  const { stdout } = await exec(`git log --format=format:%H ../data/${id}.json`)
-  const commitList: string[] = stdout.split('\n');
+  const { stdout } = await exec(`git log --format=format:"%H;%cs" ../data/${id}.json`)
+  const commitDataArr: string[] = stdout.split('\n');
 
   return (
-    <section className="container max-w-5xl mx-auto">
-      {commitList.map((item: any) => {
+    <section className="container max-w-5xl mx-auto pb-10">
+      {commitDataArr.map((item: any) => {
+        const [sha, date] = item.split(';')
         return (
-        <EntryTile entryID={id} commitSha={item} key={item} />
+          <React.Fragment key={item}>
+            <h2 className="text-green-700 font-bold text-sm md:text-base pb-3 pt-10">
+              {item === commitDataArr[0] ? `Changelog of ${id}` : date}
+            </h2>
+            <EntryTile entryID={id} commitSha={sha} />
+          </React.Fragment>
         )
       }
       )}
