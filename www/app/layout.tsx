15b12ccf48e2036182f55d09d04d21319de3ee2e
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-11-02 13:39:15 +0000

    Add Plausible analytics component to layout

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import Plausible from "@/components/Plausible";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
-          async
-          defer
-          onload="this.setAttribute('data-domain',window.location.host)"
-          src="https://plausible.io/js/plausible.js"
-        ></script>
+        <Plausible />
       </head>
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
